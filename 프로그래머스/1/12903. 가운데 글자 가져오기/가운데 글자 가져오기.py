4abb03b51147397683e732ca845d0b10e685079a
Moongeonhee <114344982+moon-gh@users.noreply.github.com>
2026-01-11 05:41:43 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    
+    l = len(s)
+    
+    if l%2 == 1:
+        answer += s[l//2]
+    else:
+        answer += s[l//2 -1]
+        answer += s[l//2]
+    
+    return answer
